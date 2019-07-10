f86593c7c2ea76bdeb1593f413bf9b3427f3fb8b
l-kya <15342795158@163.com>
2019-07-10 17:53:18 +0800

    Fix broken editing article spec

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  # before_action :authenticate_user!, expect: [:index,:show]
+  before_action :authenticate_user!, expect: [:index,:show]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
