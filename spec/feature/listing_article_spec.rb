a359fde7fd9bb386c4d758753d6745975b2d7ab4
l-kya <15342795158@163.com>
2019-07-11 11:29:40 +0800

    Show new article button to signed in users

--- spec/feature/listing_article_spec.rb
+++ spec/feature/listing_article_spec.rb
@@ -1,15 +1,14 @@
 require "rails_helper"
 RSpec.feature "Listing Articles" do
   before do
-    john = User.create(email: "john@example.com", password: "password")
+    @john = User.create(email: "john@example.com", password: "password")
     @article1 = Article.create(title: "The first article",
-                               body: "Lorem ipsum dolor sit amet, consectetur.",user:john)
+                               body: "Lorem ipsum dolor sit amet, consectetur.",user:@john)
     @article2 = Article.create(title: "The second article",
-                               body: "Pellentesque ac ligula in tellus feugiat.",user:john)
+                               body: "Pellentesque ac ligula in tellus feugiat.",user:@john)
   end
 
-  scenario "A user lists all articles" do
-
+  scenario "with articles created and user not signed in" do
     visit "/"
     expect(page).to have_content(@article1.title)
     expect(page).to have_content(@article1.body)
@@ -17,7 +16,22 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
   end
+
+  scenario "with articles created and user not signed in" do
+    login_as(@john)
+    visit "/"
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
+  end
+
+
 
   scenario "A user has no articles" do
     Article.delete_all
--- spec/feature/show_article_spec.rb
+++ spec/feature/show_article_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 RSpec.feature "Showing an Article" do
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    login_as(john)
+    @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email:"fred@example.com",password:"password")
     @article = Article.create(title: "The first article",
                               body: "Lorem ipsum dolor sit amet, consectetur.", user:john )
     end
@@ -14,4 +14,22 @@ RSpec.feature "Showing an Article" do
       expect(page).to have_content(@article.body)
       expect(current_path).to eq(article_path(@article))
     end
+
+  scenario "to a non-signed in user hides Edit/Delete Links" do
+    visit "/"
+    click_link @article.title
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
+
+  scenario "A non-owner signed in cannot see both links" do
+    login_as(@fred)
+    visit "/"
+    click_link @article.title
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+end
