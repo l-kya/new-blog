5963f02d301ccaf7b68b519270efd4a0b5587f1d
l-kya <15342795158@163.com>
2019-07-11 11:46:18 +0800

    hide links from non-signed in users and non-owners

--- spec/feature/show_article_spec.rb
+++ spec/feature/show_article_spec.rb
@@ -4,15 +4,7 @@ RSpec.feature "Showing an Article" do
     @john = User.create(email: "john@example.com", password: "password")
     @fred = User.create(email:"fred@example.com",password:"password")
     @article = Article.create(title: "The first article",
-                              body: "Lorem ipsum dolor sit amet, consectetur.", user:john )
-    end
-
-    scenario "A user lists all articles" do
-      visit "/"
-      click_link @article.title
-      expect(page).to have_content(@article.title)
-      expect(page).to have_content(@article.body)
-      expect(current_path).to eq(article_path(@article))
+                              body: "Lorem ipsum dolor sit amet, consectetur.", user:@john )
     end
 
   scenario "to a non-signed in user hides Edit/Delete Links" do
